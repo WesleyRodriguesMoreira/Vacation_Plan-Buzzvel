a860d4509c3155270984dfd28e615c8ab443ac7e
Wesley Rodrigues <wesleyrodriguesmoreira02@gmail.com>
2024-05-04 00:17:20 -0300

    .

--- Holiday_PlannerAPI/tests/Unit/UserTest.php
+++ Holiday_PlannerAPI/tests/Unit/UserTest.php
@@ -1,43 +1,49 @@
 <?php
+
 namespace Tests\Unit;
+
 use App\Http\Controllers\UserController;
 use App\Http\Requests\UserRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use PHPUnit\Framework\TestCase;
 
-class UserTest extends TestCase{
+class UserTest extends TestCase
+{
+    use RefreshDatabase; 
 
-    use RefreshDatabase;
+    protected $app; // Definir a variável $app como uma propriedade protegida
 
-    /** @var function testStoreUser Criar um teste de cadastro de usuário  */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Inicialize a aplicação Laravel
+        $this->app = require __DIR__.'/../../bootstrap/app.php';
+        $this->app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+    }
+
     public function testStoreUser(){
 
-        /** @var strin $user Receber os dados da model user */
-        $user = new UserController();
-        $request = new UserRequest([
+        // Simular uma requisição HTTP com os dados do usuário
+        $request = UserRequest::create('/store', 'POST', [
             'name' => 'Olá, Mundo!',
             'email' => 'olamundo@gmail.com',
             'password' => 'senha123',
-        ]);
-
-        $request->merge([
             'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
+            // 'remember_token' => Str::random(10),
         ]);
 
         // Desabilitar o log
         Log::shouldReceive('info')->andReturnNull();
 
-        $response = $user->store($request);
+        // Executar o método store do UserController com a requisição simulada
+        $response = $this->app->make(UserController::class)->store($request);
 
-        // Retorna uma mensagem de sucesso
+        // Verificar se a resposta indica sucesso
         $this->assertEquals(201, $response->status());
         $this->assertEquals('Usuário cadastrado com sucesso', $response->getData()->message);
     }
-
-
-    
 }
+
